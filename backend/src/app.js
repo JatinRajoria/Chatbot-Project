ff0527acb9d269244ceaace43aaa9b52cc618194
JatinRajoria <115098932+JatinRajoria@users.noreply.github.com>
2026-02-02 17:28:04 +0530

    backend functionality

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,12 +3,14 @@ const cookieParser = require('cookie-parser');
 
 const authRoutes = require('./routes/auth.routes');
 const chatRoutes = require('./routes/chat.routes');
+const path = require('path');
 
 
 const app = express();
 const cors = require('cors');
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'../public')))
 app.use(cookieParser());
 app.use(cors({
     origin: 'http://localhost:5173',
@@ -17,5 +19,9 @@ app.use(cors({
 
 app.use('/api/auth', authRoutes);
 app.use('/api/chat', chatRoutes);
+
+app.get('*name',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../public/index.html'))
+})
 
 module.exports = app;
